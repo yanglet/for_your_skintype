6b4ca032fb2aa0130116dd0a1a01d8b82f70d3b7
yanglet <y2yone023@naver.com>
2022-04-13 04:20:53 +0900

    #1, 환경설정

---
+++ src/main/java/com/project/fyst/FystApplication.java
@@ -0,0 +1,13 @@
+package com.project.fyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FystApplication.class, args);
+	}
+
+}
